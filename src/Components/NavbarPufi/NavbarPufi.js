f0db0622ad00c0925c25724928fd470b0150a932
Your Name <mareliza16@hotmail.com>
2022-08-24 18:54:30 -0500

    se termina la maquetación del primer componente

--- src/Components/NavbarPufi/NavbarPufi.js
+++ src/Components/NavbarPufi/NavbarPufi.js
@@ -1,8 +1,9 @@
-import pufi from "../../AssetsImages/pufi.jpg";
+import pufi from "../../AssetsImages/pufi.png";
 import puff from "../../AssetsImages/puff.png";
 import rain from "../../AssetsImages/img-sombrilla.png";
 import cart from "../../AssetsImages/bolsa-organica.png";
 import nap from "../../AssetsImages/almohada-de-cuello.png";
+import flecha from "../../AssetsImages/flecha-hacia-abajo.png";
 
 const NavbarPufi = () => {
   return (
@@ -10,10 +11,13 @@ const NavbarPufi = () => {
       <div className="container-menu">
         <ul className="nav-menu-one">
           <li className="nav-menu-item-one">
-            <a className="nav-menu-link-one">Mi Cuenta</a>
+            <a className="nav-menu-link-one" id="cuenta">
+              MI CUENTA{" "}
+              <img src={flecha} className="flecha" alt="flecha hacía abajo" />
+            </a>
           </li>
           <li className="nav-menu-item">
-            <a className="nav-menu-link">Mi Compra</a>
+            <a className="nav-menu-link-one">MI COMPRA</a>
           </li>
         </ul>
         <nav className="nav">
@@ -22,24 +26,29 @@ const NavbarPufi = () => {
           <ul className="nav-menu">
             <li className="nav-menu-item">
               <img src={puff} className="logo-puff" alt="puff" />
-              <a className="nav-menu-link">Pufi Puff</a>
+              <a className="nav-menu-link">PUFI PUFF</a>
             </li>
             <li className="nav-menu-item">
               <img src={rain} className="logo-rain" alt="sombrilla" />
-              <a className="nav-menu-link">Pufi Rain</a>
+              <a className="nav-menu-link">PUFI RAIN</a>
             </li>
             <li className="nav-menu-item">
               <img src={cart} className="logo-cart" alt="bolsa-mercado" />
-              <a className="nav-menu-link">Pufi Cart</a>
+              <a className="nav-menu-link">PUFI CART</a>
             </li>
             <li className="nav-menu-item">
               <img src={nap} className="logo-nap" alt="almohada-viaje" />
-              <a className="nav-menu-link">Pufi Nap</a>
+              <a className="nav-menu-link">PUFI NAP</a>
             </li>
           </ul>
         </nav>
       </div>
-      <h1 className="titulo">ESTÁR CÓMODO, NUNCA FUE TAN FÁCIL.</h1>
+      <h1 className="titulo">
+        ESTÁR CÓMODO,
+        <br />
+        NUNCA FUE TAN FÁCIL.
+      </h1>
+      <p className="parrafo">SHOP</p>
     </div>
   );
 };
