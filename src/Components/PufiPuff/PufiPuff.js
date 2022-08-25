2c46695cc9a8eca237c58ed0cbf8ade70b99d4d5
Your Name <mareliza16@hotmail.com>
2022-08-25 05:50:13 -0500

    se hace el estilo del componente pull

--- src/Components/PufiPuff/PufiPuff.js
+++ src/Components/PufiPuff/PufiPuff.js
@@ -1,7 +1,21 @@
+import puffUno from "../../AssetsImages/puffpequeño.jpg";
+import puffDos from "../../AssetsImages/puff.webp";
 export default function PufiPuff() {
   return (
-    <div>
-      <h1>Soy PufiPuff</h1>
+    <div className="container-puff">
+      {" "}
+      <div className="grupo-puff">
+        <img src={puffUno} className="puff-uno" alt="puff" /> <h3>PUFI NAP</h3>
+        <hr className="linea-puff" />
+        <p className="parrafo-puff">
+          Descripción del producto. Este es <br /> un texto simulado
+        </p>
+        <p className="parrafo-puff"> VER MAS</p>
+      </div>
+      <div>
+        {" "}
+        <img src={puffDos} className="pull-dos" alt="almohada de viaje" />
+      </div>
     </div>
   );
 }
